f663166a1dba07d1b3baa179d777b44a7c6a1e01
gongchenyu <1924639449@qq.com>
2020-04-08 15:30:08 +0800

    第一次

---
+++ app/src/main/java/com/example/neusoftfood_17110100219gcy/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.neusoftfood_17110100219gcy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
